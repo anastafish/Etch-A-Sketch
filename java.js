117d88d5e660c367a2f0dd7e3bce63db0c8e7dce
anastafish <anastafish24@gmail.com>
2022-12-04 15:32:55 +0300

    made the structure of the game

---
+++ java.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('#container')
+newGrids = document.querySelector('#newGrid')
+
+
+newGrids.addEventListener('click', () => {
+    const resultion = prompt("Enter the resultion of the grids:  ")
+    makeGrids(resultion)
+} )
+
+
+function makeGrids(resultion=16) {
+for (let i = 0; i <= resultion * resultion; i++){
+    const gridElement = document.createElement('div')
+    gridElement.classList.add('grids')
+    gridElement.addEventListener('mouseover', changeColor)
+    gridElement.addEventListener('mousedown', changeColor)
+    container.appendChild(gridElement)  
+}}
+
+function changeColor(e){
+    let num = Math.round(Math.random() * 10)
+    let currentColor;
+    if (num <= 3) currentColor = 'red'
+    else if (num > 3 && num <= 6) currentColor = 'green'
+    else {currentColor = 'blue'}  
+    e.target.style.backgroundColor = currentColor
+    console.log(currentColor)
+    
+}
+
+makeGrids()
+
+
+
